80e54743a4a41f7f07c3838d6819034ec33b0333
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-04 11:01:33 -0800

    cleaned up server.js file

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 // IMPORTS 
 const express = require("express");
-
-// CREATE APPLICATION OBJECT
 const app = express();
 
 //APP DEPENDENCIES
 const cors = require('cors')
 const morgan = require('morgan')
 
-//CONTROLLER IMPORT
-
 // INITIALIZE .ENV VARIABLES
 require("dotenv").config();
 require("./config/db.connection")
 
-// pull PORT from .env, give default value of 4000 and establish DB Connection
+//CONTROLLER IMPORT
+
 const { PORT, MONGODB_URI } = process.env;
 
 //CORS HELPER FUNCTION
 app.use(cors())
-
 app.use(morgan('dev'))
 
-
-	
 app.get("/", (req, res) => {
     res.send("hello world");
 });
