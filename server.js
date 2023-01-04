564aa6c95c08f0d56bef957823103f765d7996f4
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-04 10:46:56 -0800

    Set up boilet plate, created server.js, verified hello world in localhost4000

---
+++ server.js
@@ -0,0 +1,32 @@
+// IMPORTS 
+const express = require("express");
+
+// CREATE APPLICATION OBJECT
+const app = express();
+
+//APP DEPENDENCIES
+const cors = require('cors')
+const morgan = require('morgan')
+
+//CONTROLLER IMPORT
+
+// INITIALIZE .ENV VARIABLES
+require("dotenv").config();
+
+
+// pull PORT from .env, give default value of 4000 and establish DB Connection
+const { PORT, MONGODB_URI } = process.env;
+
+//CORS HELPER FUNCTION
+app.use(cors())
+
+app.use(morgan('dev'))
+
+
+	
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+// LISTENER
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
