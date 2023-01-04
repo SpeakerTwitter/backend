426908de053a38220f45841f30b6a846c1429f34
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-04 13:38:14 -0800

    Show route tested working in postman

--- controllers/twitter-controller.js
+++ controllers/twitter-controller.js
@@ -6,12 +6,28 @@ require("../config/db.connection");
 
 // INDEX ROUTE
 router.get("/", async (req, res) => {
-	res.status(200).json({message: "people index route"})
+	res.status(200).json({message: "twitter index route"})
 });
 
 // PEOPLE CREATE ROUTE
 router.post("/", async (req, res) =>  {
 	res.status(200).json({message: "Tweet create route"})
 });
+
+// PEOPLE SHOW ROUTE
+router.get("/:id", async (req, res) => {
+	res.status(200).json({message: "people show route: " + req.params.id })
+});
+
+// // PEOPLE DELETE ROUTE
+// router.delete("/:id", async (req, res) => {
+// 	res.status(200).json({message: "people delete route: " + req.params.id })
+// });
+
+// // PEOPLE UPDATE ROUTE
+// router.put("/:id", async (req, res) => {
+// 	console.log(req.body)
+// 	res.status(200).json({message: "people update route: " + req.params.id })
+// });
 
 module.exports = router
