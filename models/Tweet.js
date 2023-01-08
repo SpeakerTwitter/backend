dec7605b0e916cf788dd61f5a5e517876c6ffb16
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-08 13:15:45 -0600

    changed schema to test a connection between the two schemas

--- models/Tweet.js
+++ models/Tweet.js
@@ -4,6 +4,10 @@ const TweetSchema = new mongoose.Schema({
   name: {type: String, required: true},
   title: String,
   image: String,
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
 },{timestamps: true});
 
 const Tweet = mongoose.model("Tweet", TweetSchema);
