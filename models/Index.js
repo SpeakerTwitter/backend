d98b404935f2f12de9f2689439bec05e199727ec
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-10 13:40:47 -0800

    removed connection string from index.js, works in broswer

--- models/Index.js
+++ models/Index.js
@@ -1,8 +1,5 @@
-// const mongoose = require ('mongoose')
 
 module.exports = {
     Tweet: require('./Tweet'),
     Comment: require('./Comment')
 };
-
-// mongoose.connect( process.env.MONGODB_URI || "mongodb+srv://gabriel:gabriel@cluster0.uakbioc.mongodb.net/twitter2?retryWrites=true&w=majority" );
